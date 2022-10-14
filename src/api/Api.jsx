c9cabbd0f607af8d070992012c6c6b9e633be8b2
sara <sarabarati045@gmail.com>
2022-10-15 02:32:47 +0330

    feat:clean api

--- src/api/Api.jsx
+++ src/api/Api.jsx
@@ -6,14 +6,7 @@ export const geoApiOptions = {
   },
 };
 
-
 export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
-
-
-
 
 export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
 export const WEATHER_API_KEY = "fad68b261d290dc08a9d2360f023351a";
-
-
-
